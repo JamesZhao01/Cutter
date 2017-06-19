8dacfa0a4245ae03833dfcfad78b940a6d054c7d
James Zhao <jamezrollz@gmail.com>
2017-06-19 16:03:11 -0700

    Original files

---
+++ src/Fisher.java
@@ -0,0 +1,23 @@
+/**
+	* Fisher.java
+	* James Zhao
+	* Jun 19, 2017
+**/
+/**
+ * 
+ */
+
+/**
+ * @author jameszhao
+ *
+ */
+public class Fisher {
+
+    /**
+     * 
+     */
+    public Fisher() {
+	System.out.println("initiated");
+    }
+
+}
